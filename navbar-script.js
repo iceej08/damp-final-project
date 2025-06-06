a8c54b64cd17f39994d150aa48fe2408e5574bfe
iceej08 <janicekusumaaa@gmail.com>
2025-06-06 23:49:24 +0700

    add resep

---
+++ navbar-script.js
@@ -0,0 +1,15 @@
+// navbar-script.js
+const toggleBtn = document.getElementById("toggle-btn");
+const toggleIcon = document.getElementById("toggle-icon");
+const sidebar = document.getElementById("sidebar");
+
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("expand");
+
+  // Ganti ikon berdasarkan status expand
+  if (sidebar.classList.contains("expand")) {
+    toggleIcon.textContent = "menu_open";
+  } else {
+    toggleIcon.textContent = "menu";
+  }
+});
